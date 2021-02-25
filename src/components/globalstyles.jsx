aa415448fed7f260f4e3d5bf29a340fc60cde191
Lucas Lee <luc4sl33@gmail.com>
2021-02-25 22:07:56 +0900

    Fluid type

--- src/components/globalstyles.jsx
+++ src/components/globalstyles.jsx
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 16px;
+    font-size: clamp(10px, 3vw, 16px);
+  }
+
   body {
     margin: 0;
   }
