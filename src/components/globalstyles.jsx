56a1f44d0f71eeb24fb62a2507d0f10352aaa805
Lucas Lee <luc4sl33@gmail.com>
2021-02-26 17:47:52 +0900

    Edit global

--- src/components/globalstyles.jsx
+++ src/components/globalstyles.jsx
@@ -3,11 +3,19 @@ import { createGlobalStyle } from "styled-components"
 const GlobalStyle = createGlobalStyle`
   html {
     font-size: 16px;
-    font-size: clamp(10px, 3vw, 16px);
+    font-size: clamp(12px, 3vw, 16px);
+    background: #ffffff;
   }
 
   body {
     margin: 0;
+    color: #000000;
+    -webkit-font-smoothing: antialiased;
+    scroll-behavior: smooth;
+
+    *::selection {
+      background: rgba(100,100,100,0.2);
+    }
   }
 `
 
