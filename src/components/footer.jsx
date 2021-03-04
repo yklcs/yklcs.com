b5cdd0ef7c408eef45bc2d4a04bfafd7b025010f
Lucas Lee <luc4sl33@gmail.com>
2021-03-04 22:19:02 +0900

    Update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,51 @@
 import React from "react"
-import styled from "styled-components"
+import { Link } from "gatsby"
+import styled, { css } from "styled-components"
 
-const FooterDiv = styled.div`
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
   margin: 5rem 0;
   color: #a0a0a0;
 `
 
-const Footer = () => (
-  <FooterDiv>
-    <span>— Lucas Yunkyu Lee 2021.</span>
-  </FooterDiv>
+const linkStyle = css`
+  margin: 0 -0.4rem;
+  padding: 0.3rem 0.4rem;
+  color: #a0a0a0;
+  font-size: 1em;
+  text-decoration: none;
+  background: none;
+  border: none;
+  border-radius: 0.6rem;
+  outline: inherit;
+  cursor: pointer;
+  transition: background 0.3s;
+
+  &:hover {
+    background: #f0f0f0;
+  }
+`
+
+const ALink = styled.a`
+  ${linkStyle}
+`
+
+const InternalLink = styled(Link)`
+  ${linkStyle}
+`
+
+const HeaderText = styled.p`
+  margin: 0 0 0.3rem 0;
+`
+
+const Footer = ({ location }) => (
+  <FooterContainer>
+    <HeaderText>— Lucas Yunkyu Lee 2021</HeaderText>
+    {location.pathname !== "/" && <InternalLink to="/">Home</InternalLink>}
+    <ALink href="https://github.com/rocketll/luc.li">Site Source</ALink>
+  </FooterContainer>
 )
 
 export default Footer
