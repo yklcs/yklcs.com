ea8fd8095b1c2bb956c4d36578d60155ea7233ac
Lucas Yunkyu Lee <me@luc.li>
2021-08-03 02:37:33 +0900

    fix: use string mapped type for theme

--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -4,16 +4,10 @@ import { PrismTheme } from "prism-react-renderer"
 declare module "styled-components" {
   export interface DefaultTheme {
     background: {
-      // default: string,
-      // selection: string,
-      // highlight: string,
-      // sub: string,
       [key: string]: string
     }
     foreground: {
-      default: string
-      sub: string
-      link: string
+      [key: string]: string
     }
     syntax: PrismTheme
   }
