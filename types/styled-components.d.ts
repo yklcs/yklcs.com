95ee3fe68f5ee4ab87c01474e77ce6fa064b749c
Lucas Yunkyu Lee <me@luc.li>
2021-08-06 00:25:02 +0900

    fix: fix css props type error

--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -1,5 +1,6 @@
 import "styled-components"
 import { PrismTheme } from "prism-react-renderer"
+import {} from "styled-components/cssprop"
 
 declare module "styled-components" {
   export interface DefaultTheme {
