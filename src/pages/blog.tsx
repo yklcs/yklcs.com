6f4008d1b101ec1b0ea01813796f6707f6c641a1
Lucas Yunkyu Lee <me@luc.li>
2021-08-08 03:10:36 +0900

    feat: redesign blog list page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,24 +1,33 @@
-import React, { FunctionComponent } from "react"
+import React, { Dispatch, FunctionComponent, SetStateAction } from "react"
 import styled from "styled-components"
 import { Link, useStaticQuery, graphql } from "gatsby"
 
 import SEO from "../components/seo"
+import { groupBy } from "../utils/collections"
+import { useState } from "react"
+import { format, getYear } from "date-fns"
+
+interface PostNode {
+  frontmatter: {
+    title: string
+    tags: string[]
+    date: string
+  }
+  slug: string
+  timeToRead: number
+}
 
 interface BlogQueryData {
   allMdx: {
-    nodes: {
-      frontmatter: {
-        title: string
-        tags: string[]
-        date: string
-      }
-      slug: string
-      timeToRead: number
-    }[]
+    nodes: PostNode[]
   }
 }
 
+type Group = "Year" | "Category"
+
 const Blog: FunctionComponent = () => {
+  const [group, setGroup] = useState<Group>("Year")
+
   const data: BlogQueryData = useStaticQuery(graphql`
     query BlogQuery {
       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
@@ -26,7 +35,7 @@ const Blog: FunctionComponent = () => {
           frontmatter {
             title
             tags
-            date(formatString: "LL")
+            date
           }
           slug
           timeToRead
@@ -35,70 +44,119 @@ const Blog: FunctionComponent = () => {
     }
   `)
 
-  const posts = data.allMdx.nodes
-    .filter(node => !!node.frontmatter.date)
-    .map(node => (
-      <PostLink
-        title={node.frontmatter.title}
-        tags={node.frontmatter.tags}
-        date={node.frontmatter.date}
-        slug={node.slug}
-      />
-    ))
+  const posts = data.allMdx.nodes.filter(node => !!node.frontmatter.date)
 
   return (
     <Layout>
       <Title>Blog</Title>
+      <Sorter group={group} setGroup={setGroup} />
       <SEO title="Blog" description="Lucas's Blog" />
-      <Posts>{posts}</Posts>
+      {Object.entries(
+        group === "Year"
+          ? groupBy(posts, k =>
+              getYear(new Date(k.frontmatter.date)).toString()
+            )
+          : groupBy(posts, k => k.frontmatter.tags[0])
+      )
+        .sort()
+        .reverse()
+        .map(n => {
+          return <PostCollection group={n[0]} posts={n[1]} />
+        })}
     </Layout>
   )
 }
 
+const PostCollection = ({
+  group,
+  posts,
+}: {
+  group: string
+  posts: PostNode[]
+}) => (
+  <PostGroupContainer>
+    <PostGroupName>{group}</PostGroupName>
+    <PostGroup>
+      {posts.map(node => (
+        <PostLink
+          title={node.frontmatter.title}
+          tags={node.frontmatter.tags}
+          date={new Date(node.frontmatter.date)}
+          slug={node.slug}
+        />
+      ))}
+    </PostGroup>
+  </PostGroupContainer>
+)
+
+const PostGroupName = styled.div`
+  grid-column: span 2;
+`
+
 const Layout = styled.div`
+  margin: 6rem 0 0 0;
+
+  @media screen and (max-width: 40rem) {
+    margin: 4.5rem 0 0 0;
+  }
+`
+
+const PostGroupContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 1.5rem;
-  margin: 6rem 0 0 0;
+  margin: 0 0 3rem 0;
 
   @media screen and (max-width: 50rem) {
     grid-template-columns: 1fr 1fr;
-    gap: 3rem 1rem;
+    gap: 1.5rem 1rem;
   }
 `
 
 interface PostLinkProps {
   title: string
   tags: string[]
-  date: string
+  date: Date
   slug: string
 }
 
 const PostLink = ({ title, tags, date, slug }: PostLinkProps): JSX.Element => (
   <PostLinkContainer>
-    <PostTag>{tags[0]}</PostTag>
     <PostTitle to={`/${slug}`}>{title}</PostTitle>
-    <PostDate>{date}</PostDate>
+    <PostMeta>
+      <PostDate>{format(date, "y/MM/dd")}</PostDate>—
+      <PostTags>
+        {tags.map((tag, i) => [i ? "·" : "", <span>{tag}</span>])}
+      </PostTags>
+    </PostMeta>
   </PostLinkContainer>
 )
 
-const Posts = styled.div`
+const PostGroup = styled.div`
   grid-column: span 2;
 `
 
-const PostDate = styled.time`
-  margin: 0.1rem 0;
+const PostMeta = styled.div`
+  display: grid;
+  grid-auto-columns: auto;
+  grid-auto-flow: column;
+  grid-template-rows: 1fr;
+  gap: 0.75rem;
   color: ${({ theme }) => theme.neutral.l65};
-  font-variant-numeric: tabular-nums;
-  font-size: 0.9em;
+  font-size: 1em;
+  line-height: 1.5rem;
 `
 
-const PostTag = styled.span`
-  color: ${({ theme }) => theme.neutral.l65};
-  font-weight: 700;
-  font-size: 0.75em;
-  letter-spacing: 0.01em;
-  text-transform: uppercase;
+const PostDate = styled.time`
+  font-variant-numeric: tabular-nums;
+`
+
+const PostTags = styled.div`
+  display: grid;
+  grid-auto-columns: auto;
+  grid-auto-flow: column;
+  grid-template-rows: 1fr;
+  gap: 0.25rem;
 `
 
 const Title = styled.h1`
@@ -120,12 +178,56 @@ const PostTitle = styled(Link)`
   display: block;
   margin: 0;
   color: inherit;
-  font-weight: 700;
-  font-size: 1.2em;
+  font-size: 1em;
   text-decoration: none;
 
   &:hover {
     color: ${({ theme }) => theme.neutral.l15};
+  }
+`
+
+const SorterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.75rem;
+  margin: 0.75rem 0 3rem 0;
+`
+
+const Sorter = ({
+  group,
+  setGroup,
+}: {
+  group: Group
+  setGroup: Dispatch<SetStateAction<Group>>
+}) => (
+  <SorterContainer>
+    {
+      <SorterButton active={group === "Year"} onClick={() => setGroup("Year")}>
+        Year ↓
+      </SorterButton>
+    }
+    <SorterButton
+      active={group === "Category"}
+      onClick={() => setGroup("Category")}
+    >
+      Category ↓
+    </SorterButton>
+  </SorterContainer>
+)
+
+const SorterButton = styled.button<{ active: boolean }>`
+  display: ${({ active }) => (active ? "none" : "inline")};
+  padding: 0;
+  color: ${({ theme, active = false }) => theme.neutral.l65};
+  font-size: 1em;
+  line-height: inherit;
+  background: none;
+  border: none;
+  outline: none;
+  appearance: none;
+
+  &:hover {
+    color: inherit;
   }
 `
 
