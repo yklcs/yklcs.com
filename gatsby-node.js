d312aedd69e2d90beb9b0a4b26de6a3970056306
Lucas Yunkyu Lee <me@luc.li>
2022-08-25 16:54:33 +0900

    build: use pnpm

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,19 @@
 const { createFilePath } = require("gatsby-source-filesystem")
 const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
 
+exports.onCreateWebpackConfig = ({ actions }) => {
+  const gatsbyNodeModules = require("path").resolve(
+    require("fs").realpathSync("node_modules/gatsby"),
+    ".."
+  )
+
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [gatsbyNodeModules, "node_modules"],
+    },
+  })
+}
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (
