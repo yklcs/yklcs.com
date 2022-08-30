22f49d383e0aeaf5fe10837f63998ab02e202f73
Lucas Yunkyu Lee <me@luc.li>
2022-08-30 20:28:08 +0900

    fix: add omitted file

---
+++ src/templates/mdx.tsx
@@ -0,0 +1,40 @@
+import React, { FunctionComponent } from "react"
+import { PageProps } from "gatsby"
+import { MDXProvider } from "@mdx-js/react"
+
+import SEO from "../components/seo"
+import "katex/dist/katex.min.css"
+import { Content, Header } from "../components/mdx-page"
+import { components } from "../components/mdx-components"
+import { Wrapper } from "../components/layout"
+
+interface MdxFrontmatter {
+  title: string
+  date?: string
+  tags?: string[]
+  description: string
+}
+
+const MdxLayout: FunctionComponent<
+  PageProps<null, { frontmatter: MdxFrontmatter }, null>
+> = ({ children, pageContext: { frontmatter } }) => {
+  return (
+    <>
+      <SEO
+        title={frontmatter.title}
+        article
+        date={frontmatter.date}
+        tags={frontmatter.tags}
+      />
+      <MDXProvider components={components}>
+        <Wrapper as="article">
+          <Header {...frontmatter} />
+          <Content>{children}</Content>
+        </Wrapper>
+      </MDXProvider>
+    </>
+  )
+}
+
+export { MdxFrontmatter }
+export default MdxLayout
