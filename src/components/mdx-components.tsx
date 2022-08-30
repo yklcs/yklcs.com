5d0a5419d4ca2533a78961cfe3dcb725c15d2423
Lucas Yunkyu Lee <me@luc.li>
2022-08-30 22:00:41 +0900

    feat: update mdx typography

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -5,15 +5,13 @@ import { StaticImage } from "gatsby-plugin-image"
 import { Components } from "@mdx-js/react/lib"
 import respond from "../utils/responsive"
 
-const lineHeight = 1.8
 const typeScale = 1.2
 
 const Title = styled.h1`
-  margin: ${lineHeight}rem 0 ${lineHeight / 2}rem 0;
+  margin: 1rem 0 0.5rem;
   font-weight: 700;
   font-size: 2.5em;
   font-family: XCharter, serif;
-  line-height: ${lineHeight * 1.5}rem;
   letter-spacing: -0.022em;
 `
 
@@ -22,7 +20,8 @@ const mdxComponents: Components = {}
 mdxComponents.h1 = Title
 
 mdxComponents.p = styled.p`
-  margin: 0 0 ${lineHeight}rem 0;
+  margin: 0 0 1rem;
+  line-height: 1.5;
   word-wrap: break-word;
   word-break: break-word;
   hyphens: auto;
@@ -45,29 +44,22 @@ const headerStyle = css`
 `
 
 mdxComponents.h2 = styled.h2`
-  margin: ${lineHeight * 2}rem 0 0 0;
+  margin: 2rem 0 1rem;
   font-size: ${typeScale ** 2}em;
-  line-height: ${lineHeight * 3}rem;
   letter-spacing: -0.03em;
   ${headerStyle}
-
-  &:first-of-type {
-    margin: 0;
-  }
 `
 
 mdxComponents.h3 = styled.h3`
-  margin: 0;
+  margin: 2rem 0 1rem;
   font-size: ${typeScale}em;
-  line-height: ${lineHeight * 2}rem;
   letter-spacing: -0.025em;
   ${headerStyle}
 `
 
 mdxComponents.h4 = styled.h4`
-  margin: 0;
+  margin: 2rem 0 1rem;
   font-size: 1em;
-  line-height: ${lineHeight * 2}rem;
   letter-spacing: -0.02em;
   ${headerStyle}
 `
@@ -79,12 +71,12 @@ const Sidenote = styled.span`
   right: 0;
   display: block;
   width: 40%;
-  margin: -${lineHeight}rem 0 0 0;
+  margin: -${1.125 * 1.5}rem 0 0;
   padding: 0 0 0 2rem;
   color: ${({ theme }) => theme.neutral.l65};
   font-size: 0.75em;
   font-family: "Suisse Int'l", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  line-height: ${lineHeight}rem;
+  line-height: ${1.125 * 1.5}rem;
   letter-spacing: -0.012em;
   transform: translateX(100%);
 
@@ -111,4 +103,4 @@ const components: Components = {
   StaticImage,
 }
 
-export { Title, lineHeight, components }
+export { Title, components }
