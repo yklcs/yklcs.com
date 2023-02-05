d98ab52938c196c782a67f69531431d3203f4122
Lucas Yunkyu Lee <me@luc.li>
2023-02-05 14:43:24 +0900

    initial astro rewrite

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import Layout from "./src/components/layout"
-
-const wrapPageElement = ({ element, props }) => (
-  <Layout {...props}>{element}</Layout>
-)
-
-export { wrapPageElement }
--- src/utils/strip-trailing-slash.ts
+++
@@ -1,4 +0,0 @@
-const stripTrailingSlash = (str: string): string =>
-  str !== "/" && str.endsWith("/") ? str.slice(0, -1) : str
-
-export default stripTrailingSlash
