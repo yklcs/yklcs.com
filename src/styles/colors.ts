d1a6dd57c1cd635af75853c65ca9137912ee60c7
Lucas Yunkyu Lee <me@luc.li>
2023-02-10 00:54:44 +0900

    fix colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,10 +3,10 @@ import Color from "colorjs.io"
 const neutral = new Color("lch", [50, 2, 280])
 
 const colors = {
-  "neutral-90": neutral.clone().set("lch.l", 90),
-  "neutral-70": neutral.clone().set("lch.l", 60),
-  "neutral-10": neutral.clone().set("lch.l", 10),
-  "neutral-7": neutral.clone().set("lch.l", 7),
+  "neutral-90": neutral.clone().set("lch.l", 90).to("srgb"),
+  "neutral-60": neutral.clone().set("lch.l", 60).to("srgb"),
+  "neutral-10": neutral.clone().set("lch.l", 10).to("srgb"),
+  "neutral-7": neutral.clone().set("lch.l", 7).to("srgb"),
 }
 
 export default colors
