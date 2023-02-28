75ffa6b4dbec93f24e339aa185b24d5c6955a78c
Lucas Yunkyu Lee <me@luc.li>
2023-02-28 20:34:27 +0900

    update syntax highlighting

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "astro/config"
+import { type IShikiTheme, toShikiTheme } from "shiki"
 import react from "@astrojs/react"
+
+const vscodeTheme = await fetch(
+  "https://raw.githubusercontent.com/yklcs/deol-vscode/main/themes/deol-vscode-color-theme.json"
+)
+const shikiTheme = toShikiTheme(await vscodeTheme.json())
+shikiTheme.settings[3].settings.fontStyle = "underline" // https://github.com/withastro/astro/issues/6383
+console.log(shikiTheme.settings)
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  markdown: {
+    shikiConfig: {
+      theme: shikiTheme,
+    },
+  },
 })
