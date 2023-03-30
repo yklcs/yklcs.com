922504b531c010232b890f9526c5eece6c0eff44
Lucas Yunkyu Lee <me@luc.li>
2023-03-31 02:18:51 +0900

    update blog page

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const blogCollection = defineCollection({
     date: z.date(),
     tags: z.array(z.string()),
     draft: z.boolean().optional().default(false),
+    subtitle: z.string().optional(),
   }),
 })
 
