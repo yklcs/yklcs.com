4e15343265a1c906c1a9773c9cddbc450d88be61
Lucas Yunkyu Lee <lee@yklcs.com>
2023-05-25 20:02:14 +0900

    light color theme

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,10 +4,10 @@ const c = 0.0
 const h = 240
 
 const colors = {
-  "neutral-90": chroma.oklch(0.87, c, h).hex(),
-  "neutral-60": chroma.oklch(0.6, c, h),
-  "neutral-10": chroma.oklch(0.3, c, h),
-  "neutral-7": chroma.oklch(0.18, c, h).hex(),
+  "neutral-90": chroma.oklch(0.1, c, h).hex(),
+  "neutral-60": chroma.oklch(0.7, c, h),
+  "neutral-10": chroma.oklch(0.95, c, h),
+  "neutral-7": chroma.oklch(1, c, h).hex(),
 }
 
 export default colors
