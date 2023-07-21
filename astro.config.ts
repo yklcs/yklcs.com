36d5ac23dc975185dcfdb6ca1aea7f167a000d93
Lucas Yunkyu Lee <lucas@yklcs.com>
2023-07-21 13:46:06 +0900

    code style adjustment

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ const vscodeTheme = await fetch(
   "https://raw.githubusercontent.com/yklcs/deol-vscode/main/themes/deol-dull-vscode-color-theme.json"
 )
 const shikiTheme = toShikiTheme(await vscodeTheme.json())
-shikiTheme.settings[3].settings.fontStyle = "underline" // https://github.com/withastro/astro/issues/6383
 
 // https://astro.build/config
 export default defineConfig({
