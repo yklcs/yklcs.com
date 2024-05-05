85ae568401656d8c6f070342c91bc28374ca5b36
Lucas Yunkyu Lee <lucas@yklcs.com>
2024-05-05 23:36:36 +0900

    301 blog

--- cdk/viewer-request-handler.js
+++ cdk/viewer-request-handler.js
@@ -1,9 +1,19 @@
 async function handler(event) {
 	const request = event.request
-	let uri = request.uri
+	const uri = request.uri
 
-	// Handle index.html
+	if (uri == "/blog" || uri.startsWith("/blog/")) {
+		// 301 /blog to /log
+		const newUri = "/log" + request.uri.slice(5)
+		return {
+			statusCode: 302,
+			statusDescription: "Found",
+			headers: { location: { value: newUri } },
+		}
+	}
+
 	if (uri.endsWith("/")) {
+		// Handle index.html
 		request.uri += "index.html"
 	} else if (!uri.includes(".")) {
 		request.uri += "/index.html"
