f4400d593e11748bc80e62a1d9b47d7ac3982c78
Lucas Yunkyu Lee <lucas@yklcs.com>
2026-05-17 12:02:13 +0900

    Fix CSP

--- cdk/viewer-response-handler.js
+++ cdk/viewer-response-handler.js
@@ -7,7 +7,7 @@ async function handler(event) {
 	}
 	headers["content-security-policy"] = {
 		value:
-			"default-src 'self'; img-src https: data:; media-src https: data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; font-src 'self' data:; connect-src https://yklcs.goatcounter.com/count",
+			"default-src 'self'; img-src https: data:; media-src https: data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; font-src 'self' data:; connect-src 'self' https://yklcs.goatcounter.com/count",
 	}
 	headers["x-content-type-options"] = { value: "nosniff" }
 	headers["x-frame-options"] = { value: "DENY" }
